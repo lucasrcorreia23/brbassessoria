6ed56db3121dcd90e95c9ee318bd3b6bc45852ea
Lucas Rodrigues Correia <lucas_rc15@live.com>
2023-05-21 22:06:51 -0300

    Mobile Finished

--- js/custom.js
+++ js/custom.js
@@ -13,11 +13,47 @@
 	});
 
 	// Setup mobile menu
+	// Setup mobile menu
 	$('.navbarmenumobile').on('click', function(){
 		$(this).toggleClass('baropen');
 		$('.navigation ul').toggleClass('open');
 		return false;
 	});
+
+	const $ul = document.querySelector('ul');
+	
+
+	const $menu1 = document.querySelector('.menu1')
+	const $menu2 = document.querySelector('.menu2')
+	const $menu3 = document.querySelector('.menu3')
+	const $menu4 = document.querySelector('.menu4')
+	const $navbarmenumobile = document.querySelector('.navbarmenumobile')
+
+
+	$menu1 .addEventListener('click', function()
+	{
+        $ul.classList.remove('open');
+		$navbarmenumobile.classList.remove('baropen');
+
+	}	)
+
+	$menu2 .addEventListener('click', function()
+	{
+        $ul.classList.remove('open');
+		$navbarmenumobile.classList.remove('baropen');
+	}	)
+
+	$menu3 .addEventListener('click', function()
+	{
+        $ul.classList.remove('open');
+		$navbarmenumobile.classList.remove('baropen');
+	}	)
+
+	$menu4 .addEventListener('click', function()
+	{
+        $ul.classList.remove('open');
+		$navbarmenumobile.classList.remove('baropen');
+	}	)
 
 	
 	// Setup masonry
