4972cc9c77ab909dcf67b6b644d67c1f3b7dcf8f
Lucas Rodrigues Correia <lucas_rc15@live.com>
2023-06-11 17:33:45 -0300

    Dados inseridos nos scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["index.html", "css/style.css"],
   theme: {
-    extend: {},
+    extend: {
+      height:{
+        '240': '48rem',
+      },
+    },
   },
   plugins: [],
 }
