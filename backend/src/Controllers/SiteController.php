ba4bfc68a743724964dde8e7152298c1ac791dd7
Leonardo Cidral Candeia <leocidral.c@gmail.com>
2023-07-26 18:30:40 -0300

    pagarme

--- backend/src/Controllers/SiteController.php
+++ backend/src/Controllers/SiteController.php
@@ -26,14 +26,133 @@ class SiteController
             try {
                 $post = (array)json_decode($request->getBody());
 
-                throw new Exception("Problema no pagamento");
+                if ($post["tipo"] < 3) {
+                    $plan_id = "";
 
-                $data = $post;
+                    switch ($post["tipo"]) {
+                        case 0:
+                            $plan_id = "plan_MwbNO5JtvCYVGx3o";
+                            break;
+                        case 1:
+                            $plan_id = "plan_MwbNO5JtvCYVGx3o";
+                            break;
+                        case 2:
+                            $plan_id = "plan_MwbNO5JtvCYVGx3o";
+                            break;
+                    }
+
+                    $body = json_decode('{
+                      "customer": {
+                        "name": "' . $post["nome"] . '",
+                        "email": "' . $post["email"] . '",
+                        "document": "' . $post["cpfcnpj"] . '",
+                        "document_type": "' . (strlen($post["cpfcnpj"]) > 11 ? "CNPJ" : "CPF") . '",
+                        "type": "' . (strlen($post["cpfcnpj"]) > 11 ? "company" : "individual") . '"
+                      },
+                      "card": {
+                        "billing_address": {
+                          "line_1": "' . $post["numerocobranca"] . ', ' . $post["logradouro"] . ', ' . $post["bairrocobranca"] . '",
+                          "line_2": "' . $post["complementocobranca"] . '",
+                          "zip_code": "' . $post["cep"] . '",
+                          "city": "' . $post["cidadecobranca"] . '",
+                          "state": "' . $post["estadocobranca"] . '",
+                          "country": "BR"
+                        },
+                        "number": "' . $post["numerocartao"] . '",
+                        "holder_name": "' . $post["nomecartao"] . '",
+                        "exp_month": ' . (substr($post["validade"], 0, 2) * 1) . ',
+                        "exp_year": ' . (substr($post["validade"], 2) * 1) . ',
+                        "cvv": "' . $post["cvv"] . '"
+                      },
+                      "code": "",
+                      "plan_id": "' . $plan_id . '",
+                      "payment_method": "credit_card",
+                      "start_at": "' . date('Y-m-d') . '",
+                      "installments": 1
+                    }');
+
+                    $teste = PagarMeHelper::newSubscriptions($body);
+
+                    if (array_key_exists("message", $teste)) {
+                        $mensagem = "";
+
+                        foreach ($teste["errors"] as $K => $V) {
+                            if ($V[0] == "The email field is not a valid e-mail address.") {
+                                $mensagem .= "Endereço de e-mail informado invalido.\n";
+                            } elseif ($V[0] == "The number field is not a valid card number") {
+                                $mensagem .= "Número do cartão informado invalido.\n";
+                            } else {
+                                $mensagem .= "$V[0]\n";
+                            }
+                        }
+
+                        throw new Exception($mensagem);
+                    }
+                } else {
+                    $body = json_decode('{
+                        "items": [
+                            {
+                                "amount": 9900,
+                                "description": "Relatório processual",
+                                "quantity": 1
+                            }
+                        ],
+                        "customer": {
+                            "name": "' . $post["nome"] . '",
+                            "email": "' . $post["email"] . '",
+                            "document": "' . $post["cpfcnpj"] . '",
+                            "document_type": "' . (strlen($post["cpfcnpj"]) > 11 ? "CNPJ" : "CPF") . '",
+                            "type": "' . (strlen($post["cpfcnpj"]) > 11 ? "company" : "individual") . '"
+                        },
+                        "payments": [
+                            {
+                                "payment_method": "credit_card",
+                                "credit_card": {
+                                    "recurrence": false,
+                                    "installments": 1,
+                                    "card": {
+                                        "number": "' . $post["numerocartao"] . '",
+                                        "holder_name": "' . $post["nomecartao"] . '",
+                                        "exp_month": ' . (substr($post["validade"], 0, 2) * 1) . ',
+                                        "exp_year": ' . (substr($post["validade"], 2) * 1) . ',
+                                        "cvv": "' . $post["cvv"] . '"
+                                        "billing_address": {
+                                            "line_1": "' . $post["numerocobranca"] . ', ' . $post["logradouro"] . ', ' . $post["bairrocobranca"] . '",
+                                            "line_2": "' . $post["complementocobranca"] . '",
+                                            "zip_code": "' . $post["cep"] . '",
+                                            "city": "' . $post["cidadecobranca"] . '",
+                                            "state": "' . $post["estadocobranca"] . '",
+                                            "country": "BR"
+                                        },
+                                    }
+                                }
+                            }
+                        ]
+                    }');
+
+                    $teste = PagarMeHelper::newOrder($body);
+
+                    if (array_key_exists("message", $teste)) {
+                        $mensagem = "";
+
+                        foreach ($teste["errors"] as $K => $V) {
+                            if ($V[0] == "The email field is not a valid e-mail address.") {
+                                $mensagem .= "Endereço de e-mail informado invalido.\n";
+                            } elseif ($V[0] == "The number field is not a valid card number") {
+                                $mensagem .= "Número do cartão informado invalido.\n";
+                            } else {
+                                $mensagem .= "$V[0]\n";
+                            }
+                        }
+
+                        throw new Exception($mensagem);
+                    }
+                }
             } catch (Exception $e) {
                 throw $e;
             }
 
-            $responseNew = $response->withJson($data);
+            $responseNew = $response->withStatus(200);
         } catch (Exception $e) {
             $responseNew = $response->withStatus(500)->withJson($e->getMessage());
         }
---
+++ backend/src/Helpers/PagarMeHelper.php
@@ -0,0 +1,93 @@
+<?php
+define('C_REST_WEB_HOOK_URL', 'https://api.pagar.me/core/v5/'); //url on creat Webhook
+define('C_REST_APPKEY', 'sk_test_9G8Y745sWDcY645a:pk_test_a4MV8RoFXduGWZx8'); //appkey
+
+class PagarMeHelper
+{
+    protected static function callCurl($arParams)
+    {
+        $url = C_REST_WEB_HOOK_URL . $arParams['method'];
+
+        try {
+            $headers = array(
+                'Authorization: Basic ' . base64_encode(C_REST_APPKEY),
+                'Content-Type: application/json'
+            );
+
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arParams["params"]));
+
+            $response = curl_exec($ch);
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+            if (curl_errno($ch)) {
+                $response['curl_error'] = curl_error($ch);
+            }
+
+            $result = json_decode($response, true);
+
+            curl_close($ch);
+
+            if (!empty($result['error'])) {
+                throw new Exception($result['error']);
+            }
+
+            if (!empty($response['curl_error'])) {
+                $result['error'] = 'curl_error';
+                $result['error_information'] = $response['curl_error'];
+            }
+
+            return $result;
+        } catch (Exception $e) {
+            return [
+                'error' => 'exception',
+                'error_exception_code' => $e->getCode(),
+                'error_information' => $e->getMessage(),
+            ];
+        }
+    }
+
+    public static function call($method, $params = [])
+    {
+        $arPost = [
+            'method' => $method,
+            'params' => $params
+        ];
+
+        $result = static::callCurl($arPost);
+        return $result;
+    }
+
+    public static function newSubscriptions($body)
+    {
+        try {
+            $dados = (array)PagarMeHelper::call(
+                'subscriptions',
+                $body
+            );
+
+            return $dados;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    public static function newOrder($body)
+    {
+        try {
+            $dados = (array)PagarMeHelper::call(
+                'orders',
+                $body
+            );
+
+            return $dados;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+}
