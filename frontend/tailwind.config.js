e7c7a36d96394afc61187994b7a74f5d2687f04a
lucas r. c. <lucas_rc15@live.com>
2024-03-12 21:41:46 -0300

    Terceira seção adicionada

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {colors: {
+      branddark: '#0F526C', // Exemplo de cor personalizada
+    },},
   },
   plugins: [],
 }
