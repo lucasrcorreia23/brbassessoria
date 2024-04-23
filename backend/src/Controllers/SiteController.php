d973ba67f89e4449cdbd8be71aff9a0cfb626079
Leonardo Cidral Candeia <leocidral.c@gmail.com>
2024-04-23 11:29:11 -0300

    pagarme

--- backend/src/Controllers/SiteController.php
+++ backend/src/Controllers/SiteController.php
@@ -45,74 +45,74 @@ class SiteController
                     break;
             }
 
-            // $string = '{
-            //     "items": [
-            //         {
-            //             "code": "' . $code . '",
-            //             "amount": ' . $valorPlano . ',
-            //             "description": "' . $plano . '",
-            //             "quantity": 1
-            //         }
-            //     ],
-            //     "customer": {
-            //         "name": "' . $post["pessoal"]->nome . '",
-            //         "email": "' . $post["pessoal"]->email . '",
-            //         "document": "' . $post["pessoal"]->cpfcnpj . '",
-            //         "document_type": "' . (strlen($post["pessoal"]->cpfcnpj) > 11 ? "CNPJ" : "CPF") . '",
-            //         "type": "' . (strlen($post["pessoal"]->cpfcnpj) > 11 ? "company" : "individual") . '"
-            //     },
-            //     "payments": [
-            //         {
-            //             "payment_method": "credit_card",
-            //             "credit_card": {
-            //                 "recurrence": false,
-            //                 "installments": 1,
-            //                 "card": {
-            //                     "number": "' . $post["cartao"]->numerocartao . '",
-            //                     "holder_name": "' . $post["cartao"]->nomecartao . '",
-            //                     "exp_month": ' . (substr($post["cartao"]->validade, 0, 2) * 1) . ',
-            //                     "exp_year": ' . (substr($post["cartao"]->validade, 2) * 1) . ',
-            //                     "cvv": "' . $post["cartao"]->cvv . '",
-            //                     "billing_address": {
-            //                         "line_1": "' . $post["cobranca"]->numerocobranca . ', ' . $post["cobranca"]->logradouro . ', ' . $post["cobranca"]->bairrocobranca . '",
-            //                         "line_2": "' . $post["cobranca"]->complementocobranca . '",
-            //                         "zip_code": "' . $post["cobranca"]->cep . '",
-            //                         "city": "' . $post["cobranca"]->cidadecobranca . '",
-            //                         "state": "' . $post["cobranca"]->estadocobranca . '",
-            //                         "country": "BR"
-            //                     }
-            //                 }
-            //             }
-            //         }
-            //     ]
-            // }';
+            $string = '{
+                "items": [
+                    {
+                        "code": "' . $code . '",
+                        "amount": ' . $valorPlano . ',
+                        "description": "' . $plano . '",
+                        "quantity": 1
+                    }
+                ],
+                "customer": {
+                    "name": "' . $post["pessoal"]->nome . '",
+                    "email": "' . $post["pessoal"]->email . '",
+                    "document": "' . $post["pessoal"]->cpfcnpj . '",
+                    "document_type": "' . (strlen($post["pessoal"]->cpfcnpj) > 11 ? "CNPJ" : "CPF") . '",
+                    "type": "' . (strlen($post["pessoal"]->cpfcnpj) > 11 ? "company" : "individual") . '"
+                },
+                "payments": [
+                    {
+                        "payment_method": "credit_card",
+                        "credit_card": {
+                            "recurrence": false,
+                            "installments": 1,
+                            "card": {
+                                "number": "' . $post["cartao"]->numerocartao . '",
+                                "holder_name": "' . $post["cartao"]->nomecartao . '",
+                                "exp_month": ' . (substr($post["cartao"]->validade, 0, 2) * 1) . ',
+                                "exp_year": ' . (substr($post["cartao"]->validade, 2) * 1) . ',
+                                "cvv": "' . $post["cartao"]->cvv . '",
+                                "billing_address": {
+                                    "line_1": "' . $post["cobranca"]->numerocobranca . ', ' . $post["cobranca"]->logradouro . ', ' . $post["cobranca"]->bairrocobranca . '",
+                                    "line_2": "' . $post["cobranca"]->complementocobranca . '",
+                                    "zip_code": "' . $post["cobranca"]->cep . '",
+                                    "city": "' . $post["cobranca"]->cidadecobranca . '",
+                                    "state": "' . $post["cobranca"]->estadocobranca . '",
+                                    "country": "BR"
+                                }
+                            }
+                        }
+                    }
+                ]
+            }';
 
-            // $body = json_decode($string);
+            $body = json_decode($string);
 
-            // $pagamento = PagarMeHelper::newOrder($body);
+            $pagamento = PagarMeHelper::newOrder($body);
 
-            // if (array_key_exists("message", $pagamento)) {
-            //     $mensagem = "";
+            if (array_key_exists("message", $pagamento)) {
+                $mensagem = "";
 
-            //     foreach ($pagamento["errors"] as $K => $V) {
-            //         if ($V[0] == "The email field is not a valid e-mail address.") {
-            //             $mensagem .= "Endereço de e-mail informado invalido.\n";
-            //         } elseif (
-            //             $V[0] == "The number field is not a valid card number"
-            //             || $V[0] == "The field number must be a string with a minimum length of 13 and a maximum length of 19."
-            //         ) {
-            //             $mensagem .= "Número do cartão informado invalido.\n";
-            //         } else {
-            //             $mensagem .= "$V[0]\n";
-            //         }
-            //     }
+                foreach ($pagamento["errors"] as $K => $V) {
+                    if ($V[0] == "The email field is not a valid e-mail address.") {
+                        $mensagem .= "Endereço de e-mail informado invalido.\n";
+                    } elseif (
+                        $V[0] == "The number field is not a valid card number"
+                        || $V[0] == "The field number must be a string with a minimum length of 13 and a maximum length of 19."
+                    ) {
+                        $mensagem .= "Número do cartão informado invalido.\n";
+                    } else {
+                        $mensagem .= "$V[0]\n";
+                    }
+                }
 
-            //     throw new Exception($mensagem);
-            // }
+                throw new Exception($mensagem);
+            }
 
-            // if (array_key_exists("status", $pagamento) && $pagamento["status"] == "failed") {
-            //     throw new Exception("Pagamento recusado: Revise os dados preenchidos. Utilize somente cartão físico, não aceitamos cartão virtual.");
-            // }
+            if (array_key_exists("status", $pagamento) && $pagamento["status"] == "failed") {
+                throw new Exception("Pagamento recusado: Revise os dados preenchidos. Utilize somente cartão físico, não aceitamos cartão virtual.");
+            }
 
             $email = new Email();
 
